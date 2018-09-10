79f63bdad278fd066020642f3d70f4258ddf443c
Tareq Karimi <tareqkarimi@Tareqs-MacBook-Pro.local>
2018-09-09 22:48:19 -0400

    Updated npm, played with export

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var userCreds = require('./userCreds');
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/index', function (req, res){
+    console.log(req);
     res.send("Yeah, there's nothing here yet lmao");
+    app.post('/userCreds', function (){
+        userCreds.setUserDetails("Tk01", "password123", "Tareq", "Karimi")
+    });
 })
 
 app.listen(3000, function(){
     console.log("Example app listening on port 3000!");
+    console.log(__dirname);
+    console.log(__filename);
 });
 
-app.listen(4200, function(){
-    console.log("Example app is also listening on port 4200!");
-})
---
+++ userCreds.js
@@ -0,0 +1,18 @@
+module.exports.setUserDetails = setUserDetails;
+
+
+let user = {
+    username: "", 
+    password: "", 
+    fName: "", 
+    lName: ""
+}
+
+function setUserDetails(username, password, fName, lName) {
+    user.username = username;
+    user.password = password;
+    user.fName = fName;
+    user.lName = lName;
+    console.log(user);
+    return user;
+}
